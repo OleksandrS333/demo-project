b7a1864e3dd91b50bfd17e135c5d88cdcda32f76
Alex Cheble <45234022+Alexcheble@users.noreply.github.com>
2024-09-14 02:41:47 -0400

    ad

--- src/test/java/loopcamp/CukesRunner.java
+++ src/test/java/loopcamp/CukesRunner.java
@@ -1,18 +1,1 @@
-package loopcamp;
-
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        plugin = {
-                "json:target/json-reports/cucumber.json",
-                "html:target/html-reports/cucumber-report.html",
-                "junit:target/junit/junit-report.xml",
-                "rerun:target/rerun.txt",},
-        features = "src/test/resources/features",
-        publish = true,
-        tags = "@regression or @smoke"
-)
-public class CukesRunner {}
+//er {}
